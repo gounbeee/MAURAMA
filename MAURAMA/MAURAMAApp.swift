1718dda8ddc4d28b5920dc30e066532918172993
gounbeee <gounbeee@gmail.com>
2023-04-16 00:53:05 +0900

    EDITED

--- MAURAMA/MAURAMAApp.swift
+++ MAURAMA/MAURAMAApp.swift
@@ -5,6 +5,14 @@
 //  Created by Si Young Choi on 2023/01/17.
 //
 
+// < GITHUBに関して >
+// https://gist.github.com/xirixiz/b6b0c6f4917ce17a90e00f9b60566278
+//
+// ssh -T git@github.com は重要だった。
+//
+//
+
+
 import SwiftUI
 
 @main
